4a39efc63940e45ef15b90dd6ddcfaa279b86a75
soreana <esterlinkof@gmail.com>
2015-12-21 04:29:45 +0330

    first commit

---
+++ src/main/java/client/Client.java
@@ -0,0 +1,10 @@
+package client;
+
+/**
+ * Created by sinaikashipazha on 12/21/15.
+ */
+public class Client {
+    public static void main( String [] arges ){
+
+    }
+}
---
+++ src/main/java/server/Server.java
@@ -0,0 +1,10 @@
+package server;
+
+/**
+ * Created by sinaikashipazha on 12/21/15.
+ */
+public class Server {
+    public static void main( String [] arges ){
+
+    }
+}
